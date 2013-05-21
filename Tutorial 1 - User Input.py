9aa649845cfd2e841d679406acd0111f03e58c3f
Austin Owens <sdsumechatronics@gmail.com>
2013-05-20 22:02:19 -0700

    Tutorial 1

---
+++ Tutorial 1 - User Input.py
@@ -0,0 +1,14 @@
+
+#Tutorial 1
+
+
+#Click File > New Window, which will then bring up a new blank window which you can type in.
+#Then, type the following in your new window:
+
+#my first Python program
+username = input("Hello, I'm Raspberry Pi! What is your name? ")
+print ('Nice to meet you, ' + username + ' have a nice day!')
+
+#Now click File > Save As and in the new dialogue window that pops up, call your file "hello" and click "Save".
+#To run that file:
+#Click Run > Run Module or simply press F5.
